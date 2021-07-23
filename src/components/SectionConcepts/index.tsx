bfbc0513efaf2f8eca04348e04c0d23b2b67e4dc
Daniel Silveira <danielzinhors@gmail.com>
2021-07-23 10:24:10 -0300

    ajuste no concept

--- src/components/SectionConcepts/index.tsx
+++ src/components/SectionConcepts/index.tsx
@@ -12,7 +12,7 @@ const SectionConcepts = ({ title, concept }: SectionConceptsProps) => (
       <Heading lineBottom>{title}</Heading>
       <S.List>
         {concept.map((item) => (
-          <S.Item key={item}>{item.title}</S.Item>
+          <S.Item key={item.title}>{item.title}</S.Item>
         ))}
       </S.List>
     </S.Box>
